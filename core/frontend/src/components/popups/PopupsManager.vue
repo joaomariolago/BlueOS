770681e28f318ca126f82bc23d08f0b6fc7308bc
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-03-05 09:27:10 -0300

    core: frontend: Add popup library
    * Add a statically mounted popup library to allow launching stackable
      popups and get user interaction result

---
+++ core/frontend/src/components/popups/PopupsManager.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Popup
+      v-for="popup in popups"
+      :key="popup.id"
+      :identifier="popup.id"
+      :options="popup.options"
+      :dismissed="popup.dismissed"
+      @resolve="popup.resolve"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import popups from '@/store/popups'
+
+import Popup from './Popup.vue'
+
+export default Vue.extend({
+  name: 'PopupsManager',
+  components: {
+    Popup,
+  },
+  computed: {
+    popups() {
+      return popups.popup_values
+    },
+  },
+})
+</script>
+
+<style>
+</style>
